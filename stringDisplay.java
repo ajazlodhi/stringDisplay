9ab640b99f24886e80e6f93fb0e574d31d78ec65
ajazlodhi <150616811+ajazlodhi@users.noreply.github.com>
2024-01-04 13:15:12 -0500

    Update stringDisplay.java

--- stringDisplay.java
+++ stringDisplay.java
@@ -4,11 +4,11 @@ public class stringDisplay {
             int a = 10;
             int b = 20;
             int c = 30;
-            System.out.println("a is " + a + " and b is " + b " and c is " +c);
+            System.out.println("a is " + a + " and b is " + b + " and c is " + c);
             a = a + b;
             b = a - b;
             a = a - b;
             c =  c - b;
-            System.out.println("After swapping, a is " + a + " and b is " + b "and c is " +c);
+            System.out.println("After swapping, a is " + a + " and b is " + b + "and c is " + c);
         }
 }
