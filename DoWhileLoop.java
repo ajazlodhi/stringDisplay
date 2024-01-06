317abd5eed39b99fe5b0f58946a2a2d9663ca807
ajazlodhi <150616811+ajazlodhi@users.noreply.github.com>
2024-01-06 10:05:37 -0500

    Update DoWhileLoop.java

--- DoWhileLoop.java
+++ DoWhileLoop.java
@@ -1,1 +1,16 @@
-
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int i = 60, x = 21, sum = 0;
+        do {
+            System.out.println("Value after the Do loop completed : " +i);
+            i++;
+        }
+        while (i < 5);
+        do {
+            sum += x;
+            x--;
+        }
+        while (x > 10);
+        System.out.println("Summation : " +sum);
+    }
+}
